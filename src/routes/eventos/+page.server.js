d240bcb429bdfa5fa069d1617b53c9c95ed94d3d
Gabriel Ratener <gabe9693@gmail.com>
2023-11-01 12:30:55 -0600

    Use CSR in all environments

--- src/routes/eventos/+page.server.js
+++ src/routes/eventos/+page.server.js
@@ -1,11 +1,6 @@
 import eventos from '$lib/data/eventos.yaml'
 import { env } from '$env/dynamic/private'
-import { dev } from '$app/environment';
 import { calendarEvents } from '../../lib/utils';
-
-// we don't need any JS on this page, though we'll load
-// it in dev so that we get hot module replacement
-export const csr = dev;
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
